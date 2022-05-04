f4102e9d96c4708e5f0117b7191033989548fbc6
SagarR92 <104361334+SagarR92@users.noreply.github.com>
2022-05-04 17:42:38 +0530

    update libraries

--- E-PaymentTan/src/ePayment/Lauch.java
+++ E-PaymentTan/src/ePayment/Lauch.java
@@ -1,5 +1,7 @@
 package ePayment;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,13 +9,11 @@ public class Lauch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "H:\\chrome\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Chrome\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://onlineservices.tin.egov-nsdl.com/etaxnew/tdsnontds.jsp");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.close();
-
 	}
-
 }
  
